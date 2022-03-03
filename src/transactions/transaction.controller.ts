0725dc8368281b85abb23004174b753ceb43d540
Levi <levigrenz@web.de>
2022-03-03 18:45:15 +0100

    Basic Transaction

---
+++ src/transactions/transaction.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+import { TransactionService } from "./transaction.service";
+
+@Controller('transaction')
+export class TransactionController{
+    constructor(private readonly transactionService: TransactionService){}
+
+}
