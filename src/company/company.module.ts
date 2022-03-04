bb955098f8e3a1faefaccb3e5f67e01e50d6be04
Levi <levigrenz@web.de>
2022-03-04 22:40:31 +0100

    Update company.module.ts

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
+import { UserService } from 'src/user/user.service';
 
 @Module({
+  imports: [UserService],
   providers: [CompanyService],
-  controllers: [CompanyController]
+  controllers: [CompanyController],
+  exports:[CompanyService]
 })
 export class CompanyModule {}
