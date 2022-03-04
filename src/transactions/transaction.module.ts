cca215214a519ab0f6935007ac9aab4901756e36
Levi <levigrenz@web.de>
2022-03-04 21:31:13 +0100

    Added User Database

--- src/transactions/transaction.module.ts
+++ src/transactions/transaction.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([User])],
     controllers: [TransactionController],
     providers: [TransactionService],
 })
